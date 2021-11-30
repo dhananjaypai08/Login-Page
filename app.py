036fe5e4d25e4e9c23d03183cc3456a42d44ab54
Dhananjay <dhananjay2002pai@gmail.com>
2021-12-01 01:35:31 +0530

    removing unneccessary

--- app.py
+++ app.py
@@ -2,23 +2,12 @@ from logging import debug
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_mail import Mail, Message
 import testemail
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Login.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
-
-app.config['MAIL_SERVER']='smtp.mailtrap.io'
-app.config['MAIL_PORT'] = 2525
-app.config['MAIL_USERNAME'] = '092b86b98d09e5'
-app.config['MAIL_PASSWORD'] = '1f3e390d6c406a'
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-mail = Mail(app)
 
 
 class Login(db.Model):
@@ -65,19 +54,13 @@ def check():
             actual_mail = Login.query.filter_by(email=email).first()
             if actual_mail.email==email and password==actual_mail.password:
                 num = 1
-                # to run the email you have to decrease the email security of your account
-                msg = Message('Hello',sender='dhananjay2002pai@gmail.com',recipients=[f'{actual_mail.email}'])
-                msg.body = 'You are now logged in!'
-                mail.send(msg)
-                
+        
                 return render_template('index.html',flag=num,name=actual_mail.name)
             else:
                 num = 2
                 return render_template('index.html',flag=num)
         except:
-            msg = Message('Hello',sender='2020.dhananjay.pai@ves.ac.in',recipients=[f'{actual_mail.email}'])
-            msg.body = 'You are now logged in!'
-            mail.send(msg)
+            
             num = 3
             
             return render_template('index.html',flag=num)
--- testemail.py
+++ testemail.py
@@ -7,7 +7,7 @@ Email_pass = os.environ.get('email_pass')
 
 #PASSWORD IS THE APP PASSWORD THAT YOU NEED TO CREATE
 
-def sendto(reciever):
+def sendto(reciever,name):
     with smtplib.SMTP('smtp.gmail.com',587) as smtp:
         smtp.ehlo()
         smtp.starttls()
@@ -16,7 +16,7 @@ def sendto(reciever):
         smtp.login(Email_id,Email_pass)
 
         subject = 'You are now REGISTERED'
-        body = 'Thank You for Registering!!\n\nRegards,\ndp-login'
+        body = f'Hi {name}!\nThank You for Registering!!\n\nRegards,\ndp-login'
 
         msg = f'Subject:{subject}\n\n{body}'
 
